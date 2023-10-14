69079dd508a8745c96d6f2af578c8e45ef7c9ab2
pacifiquem <Murangwa Pacifique>
2023-10-14 21:16:21 +0200

    chore: project release 1

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return <footer className="text-center py-6 font-medium">Taskr Web © pacifiquem</footer>;
+};
+
+export default Footer;
